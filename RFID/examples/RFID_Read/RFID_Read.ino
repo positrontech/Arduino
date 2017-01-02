b3146e00bfaa14646fc867b535d2a8ecf5398462
positrontech <info@positrontech.in>
2017-01-02 13:10:29 +0530

    Add files via upload

---
+++ RFID/examples/RFID_Read/RFID_Read.ino
@@ -0,0 +1,18 @@
+#include <ReceiveOnlySoftwareSerial.h>
+#include <Rfid.h>
+
+Rfid RFID(2);    //To initialize RFID at PIN 2
+
+void setup() 
+{
+  Serial.begin(9600);      //Initialize Hardware Serial to check RFID Data
+}
+
+void loop() 
+{
+  String str=RFID.read();  //Read RFID Card
+    
+  if(str!="")
+    Serial.println(str);  //Print RFID Card Number on Serial
+  
+}
